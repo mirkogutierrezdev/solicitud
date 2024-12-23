0ecf5945f26579d0c20ddb8294412538290a72e7
Mirko Gutierrez <mirko.gutierrez.dev@gmail.com>
2024-12-23 09:58:39 -0300

    funcionando

--- src/main/java/com/jpa/solicitud/solicitud/repositories/IDerivacionRepository.java
+++ src/main/java/com/jpa/solicitud/solicitud/repositories/IDerivacionRepository.java
@@ -20,6 +20,7 @@ public interface IDerivacionRepository extends JpaRepository<Derivacion, Long> {
 
     List<Derivacion> findBySolicitudId(Long solicitudId);
 
+    @SuppressWarnings("null")
     Optional<Derivacion> findById(Long id);
 
     @Query("SELECT new com.jpa.solicitud.solicitud.models.dto.VDerivacionDto(d.id, d.fechaDerivacion, " +
